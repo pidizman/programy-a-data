622b82eecfbf6b7ef2d08afac4c9dacf6b8a7eab
Filipos <83700574+pidizman@users.noreply.github.com>
2025-01-16 18:05:57 +0100

    Updated home page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      background: "#222831",
+      green: "#7bc74d",
+      white: "#eeeeee",
+      gray: "#393e46",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
